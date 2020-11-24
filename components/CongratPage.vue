4d6c24bdefbc18a6d9eecabba4fd33b0805fefad
Lam Nguyen <lamnguyen@MacBook-Pro-cua-Lam.local>
2020-11-24 10:42:26 +0700

    Fix front-end home page

--- components/CongratPage.vue
+++ components/CongratPage.vue
@@ -1,16 +1,16 @@
 <template>
   <v-container
     class="gradientBG fill-height justify-center"
-    style="height: 560px"
+    style="height: 420px"
     fluid
   >
     <v-row>
       <v-col cols="12" align="center" justify="center">
-        <h1 class="display-3 mx-4 white--text">{{ $t('congratulation') }}</h1>
+        <h1 class="display-2 mx-4 white--text">{{ $t('congratulation') }}</h1>
       </v-col>
       <v-col cols="12" align="center" justify="center" class="mt-6">
         <v-btn icon dark @click="$router.push('/')">
-          <v-icon size="65">mdi-home-outline</v-icon>
+          <v-icon size="42">mdi-home-outline</v-icon>
         </v-btn></v-col
       >
     </v-row>
